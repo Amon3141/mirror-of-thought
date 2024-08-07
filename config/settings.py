3e3dbad2e84b2ea05e1bfe309e9989fa60068bf2
Amon3141 <kizawaamon@gmail.com>
2024-08-07 14:39:21 +0900

    updated texteditor

--- config/settings.py
+++ config/settings.py
@@ -20,6 +20,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
